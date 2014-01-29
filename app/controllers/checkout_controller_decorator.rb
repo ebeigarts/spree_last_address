bd1bacab90fec9f5287cc843ab9585573713f778
Edgars Beigarts <edgars.beigarts@makit.lv>
2014-01-29 12:22:47 +0200

    Replace current_user with spree_current user

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -8,8 +8,8 @@ Spree::CheckoutController.class_eval do
     #puts "BEFORE ADDRESS2 called user= #{@order}  #{@order.email}"
     @order.bill_address , @order.ship_address = FindAddressHelper.find_address(@order.email)
     # in case none found
-    @order.bill_address ||= current_user.bill_address if current_user
-    @order.ship_address ||= current_user.ship_address if current_user
+    @order.bill_address ||= spree_current_user.bill_address if spree_current_user
+    @order.ship_address ||= spree_current_user.ship_address if spree_current_user
     @order.bill_address ||= Spree::Address.new(:country_id => Spree::Config[:default_country_id])
     @order.ship_address ||= Spree::Address.new(:country_id => Spree::Config[:default_country_id])
   end
